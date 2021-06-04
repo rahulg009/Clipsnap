266fef87ed93f16c82c9d3eae26e78fcb19301c9
rgdevelopings <63463686+rahulg009@users.noreply.github.com>
2021-06-05 00:34:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
     
 // mongoose.connect('mongodb://localhost:27017/yelp_camp_v4', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect('mongodb+srv://covid19:OA8EyAafIGbY3551@cluster0.majnc.mongodb.net/covid19?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://rgtech:KtuWP5YbG2MUNpdl@cluster0.tuitl.mongodb.net/clipsnap?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
